0000000d658107c9ebdf1d4ff0006ff8c09a5eff
Ziyun Li <me@ziyunli.dev>
2022-07-19 18:57:49 -0700

    typo

--- leetcode/blind75/two_sum.py
+++ leetcode/blind75/two_sum.py
@@ -1,4 +1,7 @@
 """
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -22,9 +25,9 @@ Output: [0,1]
 
 Constraints:
 
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
+2 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
 Only one valid answer exists.
 
 
