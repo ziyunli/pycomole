0000000f4ca942a43d43c79a071f67fa5d843379
Ziyun Li <me@ziyunli.dev>
2022-07-19 22:12:16 -0700

    product except self stuck

---
+++ tests/blind75/test_product_except_self.py
@@ -0,0 +1,6 @@
+from leetcode.blind75.product_except_self import product_except_self
+
+
+def test_product_except_self() -> None:
+    assert product_except_self([1, 2, 3, 4]) == [24, 12, 8, 6]
+    assert product_except_self([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
